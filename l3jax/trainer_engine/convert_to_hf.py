f6f2ea13c24193bce4d4501175a64608a2be86a4
Felarof <nithin.sonti@gmail.com>
2024-08-24 20:47:30 -0700

    updated convert to hf to accept params

--- l3jax/trainer_engine/convert_to_hf.py
+++ l3jax/trainer_engine/convert_to_hf.py
@@ -4,24 +4,26 @@ import math
 import os
 import shutil
 
-import numpy as np
+import flax
 import jax
 import jax.numpy as jnp
-import flax
-from flax.traverse_util import flatten_dict
+import numpy as np
 import torch
+from flax.traverse_util import flatten_dict
 from transformers import LlamaConfig, LlamaForCausalLM
 
-from .llama_model import LlamaConfig as LLaMAConfigurator
-from .checkpoint_lib import Checkpointer as StreamingCheckpointer
-from .utils import float_tensor_to_dtype
 from . import config_lib
+from .checkpoint_lib import Checkpointer as StreamingCheckpointer
+from .llama_model import LlamaConfig as LLaMAConfigurator
+from .utils import float_tensor_to_dtype
 
 config = config_lib.config_dict(
-    load_checkpoint='trainstate::/mnt/persistent-disk/hf/models--felafax--llama-3.1-8B-JAX/snapshots/ebca17f216e4c02e0f31cc47264a9d65a4f5b9a9/streaming_params',
+    load_checkpoint=
+    'trainstate::/mnt/persistent-disk/hf/models--felafax--llama-3.1-8B-JAX/snapshots/ebca17f216e4c02e0f31cc47264a9d65a4f5b9a9/streaming_params',
     output_dir='/mnt/persistent-disk/easy/e2hf/',
     llama=LLaMAConfigurator("llama3_8b"),
 )
+
 
 def match_keywords(string, positives, negatives):
     for positive in positives:
@@ -33,17 +35,15 @@ def match_keywords(string, positives, negatives):
     return True
 
 
-def load_and_convert_checkpoint(path):
-    _, flax_params = StreamingCheckpointer.load_trainstate_checkpoint(path)
-    import pdb; pdb.set_trace()
+def load_and_convert_checkpoint(flax_params):
+    # _, flax_params = StreamingCheckpointer.load_trainstate_checkpoint(path)
     flax_params = flatten_dict(flax_params['params'], sep='.')
     torch_params = {}
     for key, tensor in flax_params.items():
         if match_keywords(key, ["kernel"], ["norm", 'ln_f']):
             tensor = tensor.T
-        torch_params[key] = torch.tensor(
-            float_tensor_to_dtype(tensor, 'fp32'), dtype=torch.float16
-        )
+        torch_params[key] = torch.tensor(float_tensor_to_dtype(tensor, 'fp32'),
+                                         dtype=torch.float16)
     return torch_params
 
 
@@ -59,9 +59,8 @@ def write_json(text, path):
 
 def permute(w, n_heads, input_dim, output_dim):
     # permute for sliced rotary embedding
-    return w.view(
-        n_heads, output_dim // n_heads // 2, 2, input_dim
-    ).transpose(1, 2).reshape(output_dim, input_dim)
+    return w.view(n_heads, output_dim // n_heads // 2, 2,
+                  input_dim).transpose(1, 2).reshape(output_dim, input_dim)
 
 
 def write_model(loaded, model_path):
@@ -77,48 +76,55 @@ def write_model(loaded, model_path):
     dim = llama_config.hidden_size
     dims_per_head = dim // n_heads
     base = llama_config.rope_theta
-    inv_freq = 1.0 / (base ** (torch.arange(0, dims_per_head, 2).float() / dims_per_head))
+    inv_freq = 1.0 / (base**(torch.arange(0, dims_per_head, 2).float() /
+                             dims_per_head))
 
     param_count = 0
     index_dict = {"weight_map": {}}
     for layer_i in range(n_layers):
         filename = f"pytorch_model-{layer_i + 1}-of-{n_layers + 1}.bin"
         state_dict = {
-            f"model.layers.{layer_i}.self_attn.q_proj.weight": permute(
+            f"model.layers.{layer_i}.self_attn.q_proj.weight":
+            permute(
                 loaded[f"transformer.h.{layer_i}.attention.wq.kernel"],
                 llama_config.num_attention_heads,
                 llama_config.hidden_size,
                 llama_config.hidden_size,
             ),
-            f"model.layers.{layer_i}.self_attn.k_proj.weight": permute(
+            f"model.layers.{layer_i}.self_attn.k_proj.weight":
+            permute(
                 loaded[f"transformer.h.{layer_i}.attention.wk.kernel"],
                 llama_config.num_key_value_heads,
                 llama_config.hidden_size,
-                llama_config.hidden_size // (
-                    llama_config.num_attention_heads
-                    // llama_config.num_key_value_heads
-                ),
+                llama_config.hidden_size //
+                (llama_config.num_attention_heads //
+                 llama_config.num_key_value_heads),
             ),
-            f"model.layers.{layer_i}.self_attn.v_proj.weight": loaded[f"transformer.h.{layer_i}.attention.wv.kernel"],
-            f"model.layers.{layer_i}.self_attn.o_proj.weight": loaded[f"transformer.h.{layer_i}.attention.wo.kernel"],
-
-            f"model.layers.{layer_i}.mlp.gate_proj.weight": loaded[f"transformer.h.{layer_i}.feed_forward.w1.kernel"],
-            f"model.layers.{layer_i}.mlp.down_proj.weight": loaded[f"transformer.h.{layer_i}.feed_forward.w2.kernel"],
-            f"model.layers.{layer_i}.mlp.up_proj.weight": loaded[f"transformer.h.{layer_i}.feed_forward.w3.kernel"],
-
-            f"model.layers.{layer_i}.input_layernorm.weight": loaded[f"transformer.h.{layer_i}.attention_norm.kernel"],
-            f"model.layers.{layer_i}.post_attention_layernorm.weight": loaded[f"transformer.h.{layer_i}.ffn_norm.kernel"],
-
+            f"model.layers.{layer_i}.self_attn.v_proj.weight":
+            loaded[f"transformer.h.{layer_i}.attention.wv.kernel"],
+            f"model.layers.{layer_i}.self_attn.o_proj.weight":
+            loaded[f"transformer.h.{layer_i}.attention.wo.kernel"],
+            f"model.layers.{layer_i}.mlp.gate_proj.weight":
+            loaded[f"transformer.h.{layer_i}.feed_forward.w1.kernel"],
+            f"model.layers.{layer_i}.mlp.down_proj.weight":
+            loaded[f"transformer.h.{layer_i}.feed_forward.w2.kernel"],
+            f"model.layers.{layer_i}.mlp.up_proj.weight":
+            loaded[f"transformer.h.{layer_i}.feed_forward.w3.kernel"],
+            f"model.layers.{layer_i}.input_layernorm.weight":
+            loaded[f"transformer.h.{layer_i}.attention_norm.kernel"],
+            f"model.layers.{layer_i}.post_attention_layernorm.weight":
+            loaded[f"transformer.h.{layer_i}.ffn_norm.kernel"],
         }
 
-        state_dict[f"model.layers.{layer_i}.self_attn.rotary_emb.inv_freq"] = inv_freq
+        state_dict[
+            f"model.layers.{layer_i}.self_attn.rotary_emb.inv_freq"] = inv_freq
         for k, v in state_dict.items():
             index_dict["weight_map"][k] = filename
             param_count += v.numel()
         torch.save(state_dict, os.path.join(tmp_model_path, filename))
 
     filename = f"pytorch_model-{n_layers + 1}-of-{n_layers + 1}.bin"
-        # Unsharded
+    # Unsharded
     state_dict = {
         "model.embed_tokens.weight": loaded["transformer.wte.embedding"],
         "model.norm.weight": loaded["transformer.ln_f.kernel"],
@@ -132,7 +138,8 @@ def write_model(loaded, model_path):
 
     # Write configs
     index_dict["metadata"] = {"total_size": param_count * 2}
-    write_json(index_dict, os.path.join(tmp_model_path, "pytorch_model.bin.index.json"))
+    write_json(index_dict,
+               os.path.join(tmp_model_path, "pytorch_model.bin.index.json"))
 
     config = LlamaConfig(
         vocab_size=llama_config.vocab_size,
@@ -154,7 +161,8 @@ def write_model(loaded, model_path):
     gc.collect()
 
     print("Loading the checkpoint in a Llama model.")
-    model = LlamaForCausalLM.from_pretrained(tmp_model_path, torch_dtype=torch.float16)
+    model = LlamaForCausalLM.from_pretrained(tmp_model_path,
+                                             torch_dtype=torch.float16)
     # Avoid saving this as part of the config.
     del model.config._name_or_path
 
@@ -163,13 +171,9 @@ def write_model(loaded, model_path):
     shutil.rmtree(tmp_model_path)
 
 
-def main(argv):
+def main(config, flax_params):
     assert config.load_checkpoint != "" and config.output_dir != ""
     write_model(
-        load_and_convert_checkpoint(config.load_checkpoint),
+        load_and_convert_checkpoint(flax_params),
         model_path=config.output_dir,
     )
-
-
-if __name__ == "__main__":
-    main([])
